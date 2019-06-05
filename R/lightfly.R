1551adec32b2ca4eefc592bdccff4249aa1ff9e3
twn39 <twn39@163.com>
2019-06-05 12:51:48 +0800

    fix bug

--- R/lightfly.R
+++ R/lightfly.R
@@ -21,6 +21,7 @@ Finance <- setRefClass(
       query <- sort(params)
       query_string <- toJSON(query, auto_unbox = TRUE)
       hmac(secret_key, as.character(query_string), "sha256")
+
     },
     sort = function(x) {
       x[order(names(x))]
@@ -28,7 +29,7 @@ Finance <- setRefClass(
     fetch = function (path_info, params=list()) {
       base_url <<- "http://129.211.11.159"
       url <- paste(base_url, path_info, sep = "")
-      r <- GET(url, add_headers('X-App-Id'=app_id, 'X-Token'=getToken(params, path_info)))
+      r <- GET(url, query=params, add_headers('X-App-Id'=app_id, 'X-Token'=getToken(params, path_info)))
       content(r, 'text')
     }
   )
@@ -42,8 +43,59 @@ Stock <- setRefClass(
   "Stock",
   contains = "Finance",
   methods = list(
+
+    #' get HS300
     hs300 = function() {
       path_info <- "/api/stock/hs300"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+    #' get daily history
+    #'
+    #' @field symbol symbol
+    #' @field date_from date from
+    #' @field date_to  date to
+    daily_history = function(symbol, date_from, date_to) {
+      path_info <- "/api/stock/history/daily"
+      content <- fetch(path_info, list(
+        symbol = symbol,
+        date_from = date_from,
+        date_to = date_to
+      ))
+      read_csv(content)
+    },
+
+    #' get real time stock
+    #'
+    #' @field symbol symbol
+    stock_realtime = function(symbols) {
+      path_info = "/api/stock/realtime"
+      content <- fetch(path_info, list(
+        symbols = symbols
+      ))
+      read_csv(content)
+    },
+
+    hgs_trade_realtime = function() {
+      path_info = "/api/stock/hgs/trade/realtime"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    hgtong_top10 = function () {
+      path_info = "/api/stock/hgtong/top10"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sgtong_top10 = function () {
+      path_info = "/api/stock/sgtong/top10"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    ggtong_top10 = function () {
+      path_info = "/api/stock/ggtong/top10"
       content <- fetch(path_info)
       read_csv(content)
     }
