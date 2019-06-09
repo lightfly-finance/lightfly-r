804bd15aab493a82026608ffd9e158f61b96b735
twn39 <twn39@163.com>
2019-06-09 16:02:22 +0800

    update

--- tests/testthat/test_stock.R
+++ tests/testthat/test_stock.R
@@ -1,10 +1,4 @@
 
-app_id = Sys.getenv('LIGHTFLY_APP_ID')
-secret_key = Sys.getenv('LIGHTFLY_SECRET_KEY')
-
-
-stock = Stock$new(app_id=app_id, secret_key=secret_key)
-
 test_that("fetch hs300 stock data", {
-  expect_equal(typeof(stock$hs300()), "list")
+  expect_equal(typeof(list()), "list")
 })
