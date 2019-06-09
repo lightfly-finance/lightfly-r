f7a8eb1a717b78fc2240302a5caa0335886af4de
twn39 <twn39@163.com>
2019-06-09 16:00:05 +0800

    update

--- R/lightfly.R
+++ R/lightfly.R
@@ -52,9 +52,9 @@ Stock <- setRefClass(
     },
     #' get daily history
     #'
-    #' @field symbol symbol
-    #' @field date_from date from
-    #' @field date_to  date to
+    #' @field symbol symbol.
+    #' @field date_from date from.
+    #' @field date_to  date to.
     daily_history = function(symbol, date_from, date_to) {
       path_info <- "/api/stock/history/daily"
       content <- fetch(path_info, list(
@@ -67,7 +67,7 @@ Stock <- setRefClass(
 
     #' get real time stock
     #'
-    #' @field symbol symbol
+    #' @field symbol symbol.
     stock_realtime = function(symbols) {
       path_info = "/api/stock/realtime"
       content <- fetch(path_info, list(
@@ -98,7 +98,93 @@ Stock <- setRefClass(
       path_info = "/api/stock/ggtong/top10"
       content <- fetch(path_info)
       read_csv(content)
+    },
+
+    stock_index = function () {
+      path_info = "/api/stock/index"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sh_index_component = function () {
+      path_info = "/api/stock/component/shindex"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sh_consumption_index_component = function () {
+      path_info = "/api/stock/component/shconsumptionindex"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sh50_index_component = function () {
+      path_info = "/api/stock/component/sh50index"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sh_medicine_index_component = function () {
+      path_info = "/api/stock/component/shmedicineindex"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sz_index_component = function () {
+      path_info = "/api/stock/component/szindex"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    sz_composite_index_component = function () {
+      path_info = "/api/stock/component/szcompositeindex"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    zz500_index_component = function () {
+      path_info = "/api/stock/component/zz500index"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    indicator_main = function (symbol) {
+      path_info = "/api/stock/indicator/main"
+      content <- fetch(path_info, list(
+        symbol = symbol
+      ))
+      data <- read_csv(content)
+      na.omit(t(data))
+    },
+
+    profitability = function (symbol) {
+      path_info = "/api/stock/indicator/profitability"
+      content <- fetch(path_info, list(
+        symbol = symbol
+      ))
+      data <- read_csv(content)
+      na.omit(t(data))
+    },
+
+    solvency = function (symbol) {
+      path_info = "/api/stock/indicator/solvency"
+      content <- fetch(path_info, list(
+        symbol = symbol
+      ))
+      data <- read_csv(content)
+      na.omit(t(data))
+    },
+
+    growth_ability = function (symbol) {
+      path_info = "/api/stock/indicator/growthability"
+      content <- fetch(path_info, list(
+        symbol = symbol
+      ))
+      data <- read_csv(content)
+      na.omit(t(data))
     }
+
+
   )
 )
 
@@ -111,7 +197,29 @@ Fund <- setRefClass(
   "Fund",
   contains = "Finance",
   methods = list(
+    daily_history = function (symbol, date_from, date_to) {
+      path_info <- "/api/fund/history/daily"
+      content <- fetch(path_info, list(
+        symbol = symbol,
+        date_from = date_from,
+        date_to = date_to
+      ))
+      read_csv(content)
+    },
 
+    internet_banking = function () {
+      path_info = "/api/fund/internet/banking"
+      content <- fetch(path_info)
+      read_csv(content)
+    },
+
+    basic_info = function (symbol) {
+      path_info = "/api/fund/basic/info"
+      content <- fetch(path_info, list(
+        symbol = symbol
+      ))
+      fromJSON(content)
+    }
   )
 )
 
