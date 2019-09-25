69f43d801ab43bb5aaed5d80130fe9c406a057ae
twn39 <twn39@163.com>
2019-09-25 10:15:17 +0800

    update

--- R/lightfly.R
+++ R/lightfly.R
@@ -1,4 +1,3 @@
-
 library("httr")
 library("jsonlite")
 library("digest")
@@ -7,6 +6,7 @@ library("readr")
 #' A base Reference Class for finance.
 #'
 #' @importFrom methods new
+#' @importFrom methods setRefClass
 #' @field app_id app id.
 #' @field secret_key app secret key.
 Finance <- setRefClass(
@@ -21,7 +21,7 @@ Finance <- setRefClass(
       params$app_id = app_id
       query <- sort(params)
       query_string <- toJSON(query, auto_unbox = TRUE)
-      hmac(secret_key, as.character(query_string), "sha256")
+      digest::hmac(secret_key, as.character(query_string), "sha256")
 
     },
     sort = function(x) {
@@ -30,15 +30,15 @@ Finance <- setRefClass(
     fetch = function (path_info, params=list()) {
       base_url <<- "http://lightfly.cn"
       url <- paste(base_url, path_info, sep = "")
-      r <- GET(url, query=params, add_headers('X-App-Id'=app_id, 'X-Token'=getToken(params, path_info)))
-      content(r, 'text')
+      r <- httr::GET(url, query=params, add_headers('X-App-Id'=app_id, 'X-Token'=getToken(params, path_info)))
+      httr::content(r, 'text')
     }
   )
 )
 
 #' A Stock Reference Class.
 #'
-#' @export
+#' @exportClass Stock
 #' @field app_id app id.
 #' @field secret_key app secret key.
 Stock <- setRefClass(
@@ -50,7 +50,7 @@ Stock <- setRefClass(
     hs300 = function() {
       path_info <- "/api/stock/hs300"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
     #' get daily history
     #'
@@ -65,7 +65,7 @@ Stock <- setRefClass(
         date_to = date_to,
         lang = lang
       ))
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     #' get real time stock
@@ -76,79 +76,79 @@ Stock <- setRefClass(
       content <- fetch(path_info, list(
         symbols = symbols
       ))
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     hgs_trade_realtime = function() {
       path_info = "/api/stock/hgs/trade/realtime"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     hgtong_top10 = function () {
       path_info = "/api/stock/hgtong/top10"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sgtong_top10 = function () {
       path_info = "/api/stock/sgtong/top10"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     ggtong_top10 = function () {
       path_info = "/api/stock/ggtong/top10"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     stock_index = function () {
       path_info = "/api/stock/index"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sh_index_component = function () {
       path_info = "/api/stock/component/shindex"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sh_consumption_index_component = function () {
       path_info = "/api/stock/component/shconsumptionindex"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sh50_index_component = function () {
       path_info = "/api/stock/component/sh50index"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sh_medicine_index_component = function () {
       path_info = "/api/stock/component/shmedicineindex"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sz_index_component = function () {
       path_info = "/api/stock/component/szindex"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     sz_composite_index_component = function () {
       path_info = "/api/stock/component/szcompositeindex"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     zz500_index_component = function () {
       path_info = "/api/stock/component/zz500index"
       content <- fetch(path_info)
-      read_csv(content)
+      readr::read_csv(content)
     },
 
     indicator_main = function (symbol) {
@@ -156,7 +156,7 @@ Stock <- setRefClass(
       content <- fetch(path_info, list(
         symbol = symbol
       ))
-      data <- read_csv(content)
+      data <- readr::read_csv(content)
       na.omit(t(data))
     },
 
@@ -165,7 +165,7 @@ Stock <- setRefClass(
       content <- fetch(path_info, list(
         symbol = symbol
       ))
-      data <- read_csv(content)
+      data <- readr::read_csv(content)
       na.omit(t(data))
     },
 
@@ -194,7 +194,7 @@ Stock <- setRefClass(
 
 #' A Fund Reference Class.
 #'
-#' @export
+#' @exportClass Fund
 #' @field app_id app id.
 #' @field secret_key app secret key.
 Fund <- setRefClass(
